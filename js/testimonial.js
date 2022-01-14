8086d07d04f80d7e838d13da2b279e0cf6b4c7cd
mbhattar <mbhattar@wisc.edu>
2022-01-14 12:44:14 -0600

    updated testimonial carousel and added another page

--- js/testimonial.js
+++ js/testimonial.js
@@ -1,22 +1,27 @@
 const testimonialsList = [
     {
-        name: "Pastor Gus",
-        img: "test-1.jpg",
-        text: "\"We have to remember that when you look at the word community, the letter \"u\" is there. So we have to do this together. Not as individuals. Not as politicians, but we have to be a community bound together to help one another.\""
+        name: "Dairy Strong Conference",
+        img: "DBA.png",
+        text: "C3 will be tabling at the Dairy Strong Conference at Monona Terrace this January 19th and 20th! Hope to see you there!"
     },
     {
-        name: "Pastor Brenda",
-        img: "test-2.jpg",
-        text: "\"Our faith calls us to be part of a larger community and calls us to look out for one another and the vaccine is one of those ways we can look out for one another and not just ourselves.\""
+        name: "Dale Bender, Richland County",
+        img: "dalebender.jpg",
+        text: "\"I want to preserve my quality of life for as long as I can.\""
     },
     {
-        name: "Carolina Pinzon",
-        img: "SocialMediaVaccineQuote-Pinzon.jpg",
-        text: "\"I got vaccinated to be healthy for my family, and because it is the opportunity to help my community get stronger and overcome this hard situation.\""
+        name: "Holly Muench, Grant County",
+        img: "holly.JPG",
+        text: "\"I don't want to see what happened to us happen to anyone else.\""
     },
     {
-        name: "John Rosenow",
-        img: "SocialMediaVaccineQuote-Rosenow.jpg",
-        text: "\"Since I am now vaccinated, I am more comfortable knowing that it won't kill me, and I won't put others at risk.\""
+        name: "Ron Fruit, Richland County",
+        img: "ronfruit.jpg",
+        text: "\"I want to be able to watch my grandson grow up.\""
+    },
+    {
+        name: "New DHS and CDC Recommendations",
+        img: "prc_logo.jpg",
+        text: "DHS and CDC now recommend a COVID-19 vaccine booster dose for everyone 12 and older. The Pfizer COVID-19 vaccine is the only one recommended for 12- to 17-year-olds at this time."
     },
 ];
