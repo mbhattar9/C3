34b3497f0c5a9ef45ebe9b0eea661c19ebf49cc4
mbhattar <mbhattar@wisc.edu>
2022-05-24 15:35:15 -0500

    changing updates

--- js/testimonial.js
+++ js/testimonial.js
@@ -1,5 +1,10 @@
 const testimonialsList = [
     {
+        name: "Presenting at the WPHA Conference",
+        img: "wpha.jpg",
+        text: "C3's Mahima Bhattar and Susan Passmore will be presenting an overview of the project and lessons learned at the 2022 Annual WPHA Conference. Go to https://tinyurl.com/wphac3 to learn more!"
+    },
+    {
         name: "Representing C3 at Dairy Strong 2022",
         img: "DBAgovernor.jpg",
         text: "C3, alongside other community health organizations, connected with dairy farmers and others in the dairy industry to talk about the importance of COVID-19 vaccination. We also met Governor Tony Evers!"
